89d75f53663e1614682226a9b90fe6c25316012c
LekhyaSahithiP <lelkyasahithipanchumarthi@gmail.com>
2020-03-09 22:20:28 +0530

    Design patterns - Epam task-7

---
+++ LekhyaDesignPatterns/src/test/java/Lekhya_epamtask7/LekhyaDesignPatterns/AppTest.java
@@ -0,0 +1,20 @@
+package Lekhya_epamtask7.LekhyaDesignPatterns;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
